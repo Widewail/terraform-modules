d85270417e3637c50523cfe76d64d86a815964b8
Evan Gould <evan@widewail.com>
2021-07-30 09:55:07 -0400

    Reorganize

---
+++ modules/serverless_pipeline/serverless_pipeline.tf
@@ -0,0 +1,198 @@
+variable "source_configuration" {
+  type = object({
+    provider = string
+    owner = string
+    version = string
+    configuration = map(string)
+  })
+}
+variable "name" {
+  type = string
+}
+variable "env" {
+  type = string
+}
+resource "aws_iam_role" "code_build_role" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "iam:CreateRole"
+        Effect = "Allow"
+        Sid    = ""
+      },
+      {
+        Action = "ssm:*"
+        Effect = "Allow"
+        Sid    = ""
+      },
+    ]
+  })
+
+  tags = {
+    ENV = var.env
+  }
+}
+resource "aws_s3_bucket" "codebuild_cache" {
+  bucket = "${var.env}_${var.name}_codebuild_cache"
+}
+
+resource "aws_codebuild_project" "build" {
+  name          = "${var.name}-build-${var.env}"
+  description   = "${var.name} ${var.env} codebuild"
+  build_timeout = "5"
+  service_role  = aws_iam_role.code_build_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  cache {
+    type     = "S3"
+    location = aws_s3_bucket.codebuild_cache.bucket
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:1.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name  = "${name}-${env}-logs"
+      stream_name = "log-stream"
+    }
+  }
+
+  source {
+    type            = "CODEPIPELINE"
+  }
+
+  tags = {
+    Environment = var.env
+  }
+}
+resource "aws_codepipeline" "codepipeline" {
+  name     = "tf-test-pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    location = aws_s3_bucket.codepipeline_bucket.bucket
+    type     = "S3"
+
+    encryption_key {
+      id   = data.aws_kms_alias.s3kmskey.arn
+      type = "KMS"
+    }
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = var.source_configuration.owner
+      provider         = var.source_configuration.provider
+      version          = var.source_configuration.version
+      output_artifacts = ["source_output"]
+
+      configuration = var.source_configuration.configuration
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      version          = "1"
+
+      configuration = {
+        ProjectName = aws_codebuild_project.build.name
+      }
+    }
+  }
+}
+
+resource "aws_codestarconnections_connection" "example" {
+  name          = "example-connection"
+  provider_type = "GitHub"
+}
+
+resource "aws_s3_bucket" "codepipeline_bucket" {
+  bucket = "test-bucket"
+  acl    = "private"
+}
+
+resource "aws_iam_role" "codepipeline_role" {
+  name = "test-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "codepipeline.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name = "codepipeline_policy"
+  role = aws_iam_role.codepipeline_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect":"Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:GetObjectVersion",
+        "s3:GetBucketVersioning",
+        "s3:PutObjectAcl",
+        "s3:PutObject"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.codepipeline_bucket.arn}",
+        "${aws_s3_bucket.codepipeline_bucket.arn}/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codestar-connections:UseConnection"
+      ],
+      "Resource": "${aws_codestarconnections_connection.example.arn}"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codebuild:BatchGetBuilds",
+        "codebuild:StartBuild"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+data "aws_kms_alias" "s3kmskey" {
+  name = "alias/myKmsKey"
+}
